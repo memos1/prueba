d92ba0263d43ee989582539add4680e2bd1580ec
Guillermo Carranza Ramirez <omrelliug_13@hotmail.com>
2020-09-07 19:13:48 +0200

    tercer commit

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 use App\Helpers\App;
-<<<<<<< HEAD
-
-=======
->>>>>>> fee4ba6a0d7c7aa7894586276e9fbfc6b950d1c5
 define("_VIEW_PATH_",'app/views/');
 spl_autoload_register(function($clase){
     require_once  $clase.'.php';
