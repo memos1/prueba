6f44632d1c447c3a71b8e030eb2416d476f8e0ca
Guillermo Carranza Ramirez <omrelliug_13@hotmail.com>
2020-09-07 17:34:12 +0200

    segundo commit

--- app/helpers/app.php
+++ app/helpers/app.php
@@ -21,10 +21,10 @@ class App
         } else {
             $archivoController = 'app/controllers/' . $url[0] . '.php';
         }
+        
         if (file_exists($archivoController)) {
             
             require $archivoController;
-
             $controller = new $url[0];
             //Se obtiene el número de param
             $nparam = sizeof($url);
@@ -33,22 +33,13 @@ class App
 
             if ($nparam > 1) {
                 //hay parámetros
-                if ($nparam > 2) {
-                    $param = [];
-                    for ($i = 2; $i < $nparam; $i++) {
-                        array_push($param, $url[$i]);
-                    }
-                    $controller->{$url[1]}($param);
-                } else {
-                    // solo se llama al método
-                    $controller->{$url[1]}();
-                }
             } else {
                 // si se llama a un controlador
                 $controller->render();
             }
         } else {
-            $controller = new Errores();
+            //controllar errores
+            echo 'Error al cargar el recurso';
         }
     }
 }
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 use App\Helpers\App;
 
+define("_VIEW_PATH_",'app/views/');
 spl_autoload_register(function($clase){
     require_once  $clase.'.php';
 });
